4cffc4d3e6fb3592034d58f9dc6493d640a14692
刘伟华 <liuweihua001@chinasofti.com>
2023-03-22 19:03:39 +0800

    引入element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
+<script lang="ts" setup name="App">
+import zhCn from 'element-plus/lib/locale/lang/zh-cn';
+import { ref } from 'vue';
+
+const size = ref('medium');
+const zIndex = ref(2000);
+const locale = ref(zhCn);
+</script>
 <template>
   <div>
-    <router-view></router-view>
+    <el-config-provider :size="size" :z-index="zIndex" :locale="locale">
+      <router-view></router-view>
+    </el-config-provider>
   </div>
 </template>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,24 @@
-<script lang="ts" setup name="Login"></script>
+<script lang="ts" setup name="Login">
+import { ref } from 'vue';
+
+const email = ref('');
+const password = ref('');
+function login() {
+  console.log('login');
+}
+</script>
 <template>
   <div>
-    <h1>Hello Login!</h1>
+    <el-form>
+      <el-form-item label="Email">
+        <el-input v-model="email"></el-input>
+      </el-form-item>
+      <el-form-item label="Password">
+        <el-input v-model="password" type="password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="login">Login</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
