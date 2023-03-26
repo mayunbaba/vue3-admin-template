3903260f1fd311a32805979e25183da220842274
liuweihua <18538302307@163.com>
2023-03-26 10:10:59 +0800

    格式化入参

--- src/utils/formatHttp.ts
+++ src/utils/formatHttp.ts
@@ -30,9 +30,8 @@ function formatResponseList(list: any[]) {
 
 function insertOrUpdateRequest(url: string, data: any, type: string) {
   const id = data.id;
-  delete data.id;
-  console.log('data', data);
   if (type === 'add') {
+    data.id = null;
     return request.post(url, { data });
   } else {
     return request.put(`${url}/${id}`, { data });
